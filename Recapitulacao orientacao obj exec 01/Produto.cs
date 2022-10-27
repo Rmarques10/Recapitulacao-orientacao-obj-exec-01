57825fed215c5dd047e9b80029fda72b33a55889
Rafael Marques <rafaelmarques.rm86@gmail.com>
2022-10-27 13:14:40 -0300

    Recapitulação

---
+++ Recapitulacao orientacao obj exec 01/Produto.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recapitulacao_orientacao_obj_exec_01
+{
+    internal class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; private set; }
+        public int Quantidade { get; private set; }
+
+        public Produto()
+        {
+        }
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public double ValorTotalEmEstoques()
+        {
+            return Preco * Quantidade;
+        }
+        public void AdicionarProtudo(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProtudo(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return Nome
+                + ", $ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " Unidades, Total:"
+                + ValorTotalEmEstoques().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Recapitulacao orientacao obj exec 01/Program.cs
@@ -0,0 +1,29 @@
+﻿using Recapitulacao_orientacao_obj_exec_01;
+using System.Globalization;
+
+Console.WriteLine("Entre com os dados do produto:");
+Console.WriteLine("Nome:");
+string nome = Console.ReadLine();
+Console.WriteLine("Preço:");
+double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.WriteLine("Quantidade:");
+int quantidade = int.Parse(Console.ReadLine());
+
+Produto p = new Produto(nome, preco, quantidade);
+Console.WriteLine("Dados do produto: " + p);
+
+Console.WriteLine();
+
+Console.WriteLine("Digite o número de produtos a serem adicionados:");
+int qte = int.Parse(Console.ReadLine());
+
+p.AdicionarProtudo(qte);
+Console.WriteLine("Dados do produto: " + p);
+
+Console.WriteLine();
+
+Console.WriteLine("Digite o número de produtos a serem removidos:");
+qte = int.Parse(Console.ReadLine());
+
+p.RemoverProtudo(qte);
+Console.WriteLine("Dados do produto: " + p);
